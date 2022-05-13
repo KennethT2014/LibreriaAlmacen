7a479fafa57bad7b15cdc6a272c3b909b31a354a
kennethT2014 <kenneth.teller2014@gmail.com>
2022-05-13 16:06:27 -0600

    Add project files.

---
+++ LibreriaAlmacen/ContactoBd.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaAlmacen
+{
+    public class ContactoBd
+    {
+        string connectionString = @"Server=KENNETH;Initial Catalog = Libreria; Integrated Security = True";
+
+        public List<Libreria> Get()
+        {
+            List<Libreria> libreria = new List<Libreria>();
+
+            string queryString = "SELECT Titulo, ISBN";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Libreria alibreria = new Libreria();
+                        alibreria.Titulo = reader.GetInt32(0);
+                        alibreria.ISBN = reader.GetString(1);
+                        libreria.Add(alibreria);
+                    }
+                    reader.Close();
+                }catch (Exception ex)
+                {
+                    throw new Exception("Hay un error en la BD"+ ex.Message);
+                }
+            }
+            return libreria;
+        }
+
+        public Libreria Get(int Titulo)
+        {
+            string queryString = "SELECT Titulo, ISBN";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                Command.Parameters.AddWithValue("@Titulo", Titulo);
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    reader.Read();
+                    Libreria alibreria = new Libreria();
+                    alibreria
+                }
+            }
+        }
+    }
+}
---
+++ LibreriaAlmacen/Datos.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LibreriaAlmacen
+{
+    public class Datos
+    {
+        private string connectionString;
+
+        protected SqlConnection GetConnection()
+        {
+            connectionString = @"Server=KENNETH;Initial Catalog = Libreria; Integrated Security = True";
+            return new SqlConnection(connectionString);
+        }
+
+        public int ExecuteNonQuery(string transactSql, List<SqlParameter> parameters)
+        {
+            using (var connection = GetConnection())
+            {
+                connection.Open();
+                using (var command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandText = transactSql;
+                    command.CommandType = CommandType.Text;
+                    foreach (SqlParameter item in parameters)
+                    {
+                        command.Parameters.Add(item);
+                    }
+                    int result = command.ExecuteNonQuery();
+                    parameters.Clear();
+                    return result;
+                }
+            }
+        }
+
+        public DataTable ExecuteReader(string transactSql, List<SqlParameter> parameters)
+        {
+            using (var connection = GetConnection())
+            {
+                connection.Open();
+                using (var command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandText = transactSql;
+                    command.CommandType = CommandType.Text;
+
+                    if (parameters != null)
+                    {
+                        foreach (SqlParameter item in parameters)
+                        {
+                            command.Parameters.Add(item);
+                        }
+                    }
+
+                    SqlDataReader reader = command.ExecuteReader();
+                    using (var table = new DataTable())
+                    {
+                        if (parameters != null)
+                        {
+                            parameters.Clear();
+                        }
+
+                        table.Load(reader);
+                        reader.Dispose();
+                        return table;
+                    }
+                }
+            }
+        }
+    }
+}
